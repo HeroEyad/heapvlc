d59b37d4c62f7fe615ed0a6f0643633b637ec64b
swordcube <--local>
2026-08-17 23:51:07 -0500

    linux support :D

--- src/heapvlc/macro/Checks.hx
+++ src/heapvlc/macro/Checks.hx
@@ -1,13 +1,13 @@
 package heapvlc.macro;
 
 #if macro
+import haxe.macro.Compiler;
 import haxe.macro.Context;
 import haxe.macro.Expr;
 import haxe.io.Path;
 import sys.FileSystem;
 
 class Checks {
-
 	static var ran = false;
 
 	public static function run():Array<Field> {
@@ -18,15 +18,31 @@ class Checks {
 		return Context.getBuildFields();
 	}
 
+	static function checkPlatform():Void {
+		var supported = ["Windows", "Linux"];
+		var currentPlatform = Sys.systemName();
+
+		if(!supported.contains(currentPlatform)) {
+			Context.error(
+				"heapvlc: " + currentPlatform + " is not supported",
+				Context.currentPos()
+			);
+		} else {
+			var os = currentPlatform.toLowerCase();
+			if(!Context.defined(os))
+				Compiler.define(os);
+		}
+	}
+
 	static function checkNativeLibrary():Void {
 		var cls = Context.getLocalClass().get();
 		var modulePath = Context.resolvePath(cls.module.split(".").join("/") + ".hx");
 		var libRoot = Path.directory(Path.directory(Path.directory(modulePath)));
-		var hdll = Path.join([libRoot, "native", "bin", "vlc.hdll"]);
+		var hdll = Path.join([libRoot, "native", "bin", "vlc-" + Sys.systemName().toLowerCase() + ".hdll"]);
 		if (!FileSystem.exists(hdll)) {
 			Context.warning(
-				'heapvlc: native/bin/vlc.hdll not found (expected at $hdll). '
-				+ 'Run native/build.ps1 before running anything that uses HeapVideo/LibVLC, '
+				'heapvlc: native/bin/vlc-${Sys.systemName().toLowerCase()}.hdll not found (expected at $hdll). '
+				+ 'Run ${Sys.systemName() == "Windows" ? "native/build.ps1" : "native/build.sh"} before running anything that uses HeapVideo/LibVLC, '
 				+ 'or native calls will fail at runtime.',
 				Context.currentPos()
 			);
